2075bfa50647e45261ab3ccb5735285b195a1fd8
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-10-23 00:36:04 +0600

    Add colors.js library and implement database connection
    - Integrated colors.js library for enhanced console output styling.
    - Created example usage of colors in safe-string.js.
    - Added TypeScript definitions for colors.js in index.d.ts and safe.d.ts.
    - Implemented custom color styles and themes in the library.
    - Established MongoDB connection in db.js with error handling and success messages.
    - Configured environment variables for server and database settings.

---
+++ server/src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import colors from 'colors';
+
+const dbConnect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {});
+    console.log(`Database Connected Succefully ${conn.connection.host}`.bgBlue);
+  } catch (error) {
+    console.log(`error : ${error.message}`);
+  }
+};
+
+module.exports = dbConnect;
